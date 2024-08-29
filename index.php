54a838f1965a9bfe7831d5f04a098053134a3cb9
hackbras <hackbras@outlook.com>
2024-08-29 17:26:39 -0300

    Arquivos iniciais

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Tema Suportedesk construido no desafio 21 dias
+ *
+ * @link suportedesk.com.br
+ *
+ * @package Suportedesk
+ * @since Suportedesk 0.0.1
+ */
+
+/**
+ * Register block styles.
+ */
